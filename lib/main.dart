cd6d1ae763ccd830849ae1bba363eefcfa0c8c03
Adedoyin <isradeoluwa@gmail.com>
2020-10-22 13:15:31 +0100

    Expense Planner App: Hard Coded List Of Transaction

---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:expense_planner/transaction.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(HomePage());
+}
+
+class HomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+        id: "t1", title: "New Shoes", amount: 60.99, date: DateTime.now()),
+    Transaction(
+        id: "t2", title: "New Bags", amount: 79.99, date: DateTime.now()),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Expense Planner"),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              width: double.infinity,
+              child: Card(
+                color: Colors.blueGrey,
+                child: Text("Chart"),
+                elevation: 5,
+              ),
+            ),
+            Column(
+              children: transactions.map((trn) {
+                return Card(
+                  child: Row(
+                    children: [
+                      Container(
+                        child: Text(trn.amount.toString()),
+                      ),
+                      Column(
+                        children: [Text(trn.title), Text(trn.date.toString())],
+                      )
+                    ],
+                  ),
+                  elevation: 5,
+                );
+              }).toList(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
